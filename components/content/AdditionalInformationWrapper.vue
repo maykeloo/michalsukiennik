6d39e4ba179f693b5a72c36d632d7a564ced1f44
Michał Sukiennik <michal.sukiennik.contact@gmail.com>
2024-02-01 09:31:58 +0100

    chore: add dependencies and change font

--- components/content/AdditionalInformationWrapper.vue
+++ components/content/AdditionalInformationWrapper.vue
@@ -1,3 +1,9 @@
 <template>
-  
+  <div class="bg-blue-400/20 border-blue-400 !border border-solid p-4 rounded-lg">
+    <div class="text-blue-400 flex gap-2 font-semibold">
+      <Icon name="ri:information-line" size="25" />
+      <span>{{ $t('additionalInfo') }}</span>
+    </div>
+    <ContentSlot />
+  </div>
 </template>
